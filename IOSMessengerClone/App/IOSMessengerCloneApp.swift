0b8343755dc0fce630a7ba83474d906f5b26c480
GkhKaya <gkhkaya00@gmail.com>
2023-08-13 22:28:12 +0300

    Project setup

---
+++ IOSMessengerClone/App/IOSMessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSMessengerCloneApp.swift
+//  IOSMessengerClone
+//
+//  Created by Gokhan Kaya on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct IOSMessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOSMessengerClone/Core/Auth/View/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  IOSMessengerClone
+//
+//  Created by Gokhan Kaya on 13.08.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
